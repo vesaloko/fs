1f5f6c80851b7426a8145aaf2df26003a5eb6f70
Salokoski Venla Josefina <ven.lasalokoski@gmail.com>
2024-02-15 14:29:57 +0200

    1.7.2

--- 01/unicafe/unicafe/src/App.jsx
+++ 01/unicafe/unicafe/src/App.jsx
@@ -4,31 +4,46 @@ const App = () => {
 const feedback = 'give feedback'
 const stats = 'statistics'
   // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
+  const [rate, setRate] = useState({
+    good: 0, neutral: 0, bad:0, all:0, pos:0, avg:0})
+
+const handleGoodRate = () => {
+  setRate({...rate, good: rate.good+1, all: rate.all +1, avg:rate.avg+1})
+}
+
+const handleBadRate = () => {
+  setRate({...rate, bad: rate.bad+1, all: rate.all +1, avg: rate.avg-1})
+}
+
+
+const handleNeutralRate = () => {
+  setRate({...rate, neutral: rate.neutral+1, all: rate.all +1})
+}
 
   return (
     <div>
       <Header feedback={feedback}/>
-         <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+        <Button handleClick={handleGoodRate} text='good' />
+        <Button handleClick={handleBadRate} text='bad' />
+        <Button handleClick={handleNeutralRate} text='neutral' />
       <Header feedback={stats}/>
-      <div><p>good </p>{good}</div>
-      <div><p>neutral </p>{neutral}</div>
-      <div><p>bad </p>{bad}</div>
+     <p>good {rate.good}</p> 
+       <p>neutral {rate.neutral}</p>
+       <p>bad {rate.bad}</p>
+       <p>all {rate.all}</p> 
+      <p>average {rate.avg/rate.all} </p>
+      <p>positive {rate.good/rate.all * 100} %</p>
+    
     </div>
   )
 }
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 const Header = ({feedback}) => {
   return (
       <h1>{feedback}</h1>
