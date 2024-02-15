4e759c74aa539918c0daf5b5454775c93e8e10cf
Salokoski Venla Josefina <ven.lasalokoski@gmail.com>
2024-02-15 21:10:58 +0200

    1.14

--- 01/unicafe/unicafe/src/App.jsx
+++ 01/unicafe/unicafe/src/App.jsx
@@ -7,16 +7,21 @@ const stats = 'statistics'
 const [rate, setRate] = useState({
   good: 0, neutral: 0, bad:0, all:0, pos:0, avg:0})
 
-const anecdotes = [    'If it hurts, do it more often.',
+  const anecdotes = [
+    'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-]
-  const [selected, setSelected] = useState()
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [voted, setVoted] = useState(Array(anecdotes.length).fill(0))
+  const n = Math.max(...voted)
+  const index = voted.indexOf(n)
 
 const statsTable = {
   good: {
@@ -42,6 +47,8 @@ const statsTable = {
   pos: {
       text: 'positive',
       value: rate.good/rate.all *100,
+      unit: '%'
+      
   },
 }
 
@@ -58,34 +65,55 @@ const handleNeutralRate = () => {
 }
 
 const handleAnecdote = () => {
-  const rand = Math.floor(Math.random() * anecdotes.length);
-  setSelected(anecdotes[rand])
+  setSelected(Math.floor(Math.random() * anecdotes.length))
 }
+
+const handleAnecdoteVote = () => {
+  const newVotes = [...voted]
+  newVotes[selected] += 1
+  setVoted(newVotes)
+  }
+
   return ( 
     <div>
       <Header head ={feedback}/>
         <Button handleClick={handleGoodRate} text='good' />
         <Button handleClick={handleBadRate} text='bad' />
         <Button handleClick={handleNeutralRate} text='neutral' />
-        <Button handleClick={handleAnecdote} text='anecdote' />
-        { <Anecdotes selectedAnecdote={selected} /> }
       <Header head ={stats}/>
-      <History allClicks={rate.all} />
-      {rate.all > 0 && (<Statistics statsTable={statsTable}/>) }
-    
-
+        <History allClicks={rate.all} />
+        {rate.all > 0 && (<Statistics statsTable={statsTable}/>) }
+      <Header head='Anecdotes'/>
+        <Button handleClick={handleAnecdote} text='anecdote' />
+        <Button handleClick={handleAnecdoteVote} text='vote this anecdote' />
+        <Anecdotes anecdotes={anecdotes[selected]} votes={voted[selected]}/>
+      <Header head='the most voted anecdote'/>
+        <HighestVoted anecdotes={anecdotes[index]} n={n} />
     </div>
   )
 }
+
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}> {text} </button>
 )
 
-const Anecdotes = ({selectedAnecdote }) => {
+
+const Anecdotes = (props) => {
+  return(
+    <div>
+      <div>{props.anecdotes}</div>
+      <div>has {props.votes} votes</div>
+    </div>
+  )
+}
+
+
+const HighestVoted = (props) => {
   return (
     <div>
-      <p>{selectedAnecdote}</p>
+      <div>{props.anecdotes}</div>
+      <div>has {props.n} votes, which is the most votes at the moment</div>
     </div>
   )
 }
@@ -103,26 +131,26 @@ const Statistics = ({statsTable}) => {
   <StatisticsLine stats = {statsTable.pos}/>
       </tbody>
     </table>
-  )
-  }
-
+  )}
 
   const StatisticsLine = ({stats}) => {
     return (<tr>
         <td>{stats.text} </td>
         <td>{stats.value} </td>
+        <td>{stats.unit} </td>
     </tr>)
 }
 
 
 const Header = ({head}) => {
-  return ( <h1>{head}</h1>) }
+  return ( <h1>{head}</h1>) 
+}
 
 
-  const History = ({allClicks }) => {
-    if (allClicks === 0) {
-      return (<div> press the buttons to see the statistics </div>
-      );}};
+const History = ({allClicks }) => {
+  if (allClicks === 0) {
+    return (<div> press the buttons to see the statistics </div>
+);}};
 
 
 export default App
